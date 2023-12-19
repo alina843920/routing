7be0e74964f31cf36ffc1a4f2966c6190e1ebec0
Alina Handzha <alinahandzha@gmail.com>
2023-12-19 14:18:04 +0100

    yoo

--- routinggg/src/components/Main.jsx
+++ routinggg/src/components/Main.jsx
@@ -1,17 +1,16 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Contact from "./Contact";
 import Me from "./Me";
 import Preferences from "./Preferences";
 const Main = () => (
   <main className="main">
-    <Switch>
+    <Routes>
       <Route path="/" exact component={Dashboard} />
       <Route path="/contact" component={Contact} />
       <Route path="/me" component={Me} />
       <Route path="/preferences" component={Preferences} />
-    </Switch>
+    </Routes>
   </main>
 );
-
 export default Main;
